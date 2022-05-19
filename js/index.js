4f1f9419b3e7f1e2ebdf5af1982fe2b976decc38
Mendy <mendyw94@gmail.com>
2022-05-19 19:37:59 -0400

    Completed Lab

--- js/index.js
+++ js/index.js
@@ -1,24 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     //Loading content to page
-    fetch('http://localhost:3000/monsters/?_limit=20&_page=3')
-    .then(resp => resp.json())
-    .then(data => {
-        const cardCreator = (monster) => {
-            const newDiv = document.createElement('div')
-            const name = document.createElement('h2')
-            name.textContent = monster.name
-            const age = document.createElement('h4')
-            age.textContent = monster.age
-            const description = document.createElement('p')
-            description.textContent = monster.description
-            newDiv.append(name, age, description)
-            document.getElementById('monster-container').append(newDiv)
-        }
-        data.forEach(element => {
-            cardCreator(element)
-        });
-    })
-
+    let pageNumber = 1;
+    fetcher();
     //creating "Add Monster" form
     const form = document.createElement('form')
     const name = document.createElement('input')
@@ -34,20 +17,17 @@ document.addEventListener("DOMContentLoaded", () => {
     btn.textContent = 'Create'
     form.append(name, age, description, btn)
     form.addEventListener('submit', (e) => {
-
         e.preventDefault()
-
         function formDeets () {
             const name = document.getElementById('name').value
             const age = document.getElementById('age').value
             const description = document.getElementById('description').value
             return {
-                name: `${name}`,
-                age: `${age}`,
-                description: `${description}`
+                name: name,
+                age: age,
+                description: description
             }
         }
-
         fetch('http://localhost:3000/monsters', {
             method: 'POST',
             headers: {
@@ -58,20 +38,7 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(resp => resp.json())
         .then(data => {
             console.log(data)
-            //append data to list
-            const cardCreator = (monster) => {
-                const newDiv = document.createElement('div')
-                const name = document.createElement('h2')
-                name.textContent = monster.name
-                const age = document.createElement('h4')
-                age.textContent = monster.age
-                const description = document.createElement('p')
-                description.textContent = monster.description
-                newDiv.append(name, age, description)
-                document.getElementById('monster-container').append(newDiv)
-            }
             cardCreator(data)
-
             document.getElementById('name').value = ''
             document.getElementById('age').value = ''
             document.getElementById('description').value = ''
@@ -80,6 +47,47 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     document.getElementById('create-monster').append(form)
 
-    
+    //adding functionality to the next and previous buttons
 
+    document.getElementById('back').addEventListener('click', () => {
+        if (pageNumber > 1) {
+            pageNumber -= 1
+            fetcher()
+        }
+    })
+    document.getElementById('forward').addEventListener('click', () => {
+        if (pageNumber < 60) {
+            pageNumber += 1
+            fetcher()
+        }
+    })
+
+
+
+    function fetcher () {
+        //remove content
+        document.getElementById('monster-container').replaceChildren()
+        //fetching data
+        fetch(`http://localhost:3000/monsters/?_limit=50&_page=${pageNumber}`)
+        .then(resp => resp.json())
+        .then(data => {
+                data.forEach(monster => {
+                cardCreator(monster)
+            });
+        })
+    }
+    //creating card
+    const cardCreator = (monster) => {
+        const newDiv = document.createElement('div')
+        newDiv.id = monster.id
+        const name = document.createElement('h2')
+        name.textContent = monster.name
+        const age = document.createElement('h4')
+        age.textContent = monster.age
+        const description = document.createElement('p')
+        description.textContent = monster.description
+        newDiv.append(name, age, description)
+        document.getElementById('monster-container').append(newDiv)
+    
+    }
 })
